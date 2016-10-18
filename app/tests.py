23298f2fcb95c3a34ee0f64ff701a0497e5b6e25
Eugene Ng <eugenen@cs.utexas.edu>
2016-10-17 19:49:22 -0500

    wrote a basic skeleton for unit tests based on unit testing from prev projs

---
+++ app/tests.py
@@ -0,0 +1,47 @@
+# -------
+# imports
+# -------
+
+from unittest import main, TestCase
+
+# probably import from classes in models
+
+# ------------------
+# Setup and Teardown
+# ------------------
+
+@classmethod
+def setUpClass(cls):
+    """docstring"""
+
+@classmethod
+def tearDownClass(cls):
+    """docstring"""
+
+def setUp():
+    """docstring"""
+
+def tearDown():
+    """docstring"""
+
+# -----------------------
+# TestModels or something
+# -----------------------
+
+class TestModels(TestCase):
+
+    """docstring"""
+
+    # -------
+    # method1
+    # -------
+
+    def test_method1_1(self):
+        """"docstring"""
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    main()
