9d5d76a6198203d934ce71dc0d0032b954ac92bc
cameronreynoldson <cameronreynoldson@utexas.edu>
2016-10-20 18:52:23 +0000

    removed environment variable

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
 #!flask/bin/python
 from app import app
-from os import environ
-if environ.get('production', False):
-	app.run(debug=True,host='0.0.0.0',port=80)
-else:
-	app.run(debug=True,host='0.0.0.0')
+app.run(debug=True,host='0.0.0.0',port=80)
