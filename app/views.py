7055ed9dd3b8d0c1c1fcd31e281dfdea2a535ddc
Addy Kim <soyaddyk@gmail.com>
2016-10-20 12:46:24 -0500

    Make use of json files

--- app/views.py
+++ app/views.py
@@ -10,16 +10,14 @@ def index():
 def about():
 	return render_template('about.html', title='about')
 
-@app.route('/category')
-def category():
+@app.route('/<category>')
+def category(category):
 	"""Render table template"""
 	return render_template('category.html',title='Category')
 
-@app.route('/details/<entity>')
-def show_user_profile(entity):
-    return render_template('details.html', 
-    	title=entity,
-    	name=entity,
-    	tagline='Meow meow meow meow. I eat tuna',
-    	twitter='discoverdowning',
-    	image='http://i.imgur.com/IpZGW1j.jpg')
+@app.route('/<category>/<entity>')
+def details(category, entity):
+	filename = "static/data/%s/%s.json" % (category, entity)
+	with app.open_resource(filename) as json_data:
+		data = json.load(json_data)
+	return render_template('details.html', data=data)
