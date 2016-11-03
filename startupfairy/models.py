8466fe7dbd6e9e1bd7746463327f62f9cab62950
cameronreynoldson <cameronreynoldson@utexas.edu>
2016-11-03 00:29:46 +0000

    import changes

---
+++ startupfairy/models.py
@@ -0,0 +1,167 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from run import app
+
+# initialize database object
+db = SQLAlchemy(app)
+
+class Company(db.Model):
+    __tablename__ = "company"
+    # set column types
+    company_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    summary = db.Column(db.String, unique=True)
+    people = db.Column(db.String)
+    city = db.Column(db.String)
+    financial_orgs = db.Column(db.String)
+    twitter = db.Column(db.String)
+    website = db.Column(db.String)
+    logo_url = db.Column(db.String)
+
+    # set column values in constructor
+    #def __init__(self, name, summary, people, city, financial_orgs, twitter, website, logo_url):
+    def __init__(self, company_id, name, summary, people, city, financial_orgs, twitter, website, logo_url):
+        self.company_id = company_id
+        self.name = name
+        self.summary = summary
+        self.people = people
+        self.city = city
+        self.financial_orgs = financial_orgs
+        self.twitter = twitter
+        self.website = website
+        self.logo_url = logo_url
+
+    def __repr__(self):
+        return '<Company %r>' % self.name
+
+    def dictionary(self):
+        """Returns a dictionary representation of the class data"""
+        dict_rep = {}
+        dict_rep['company_id'] = self.company_id
+        dict_rep['name'] = self.name
+        dict_rep['summary'] = self.summary
+        dict_rep['people'] = self.people
+        dict_rep['city'] = self.city
+        dict_rep['financial_orgs'] = self.financial_orgs
+        dict_rep['twitter'] = self.twitter
+        dict_rep['website'] = self.website
+        dict_rep['logo_url'] = self.logo_url
+        return dict_rep
+
+
+class FinancialOrg(db.Model):
+    __tablename__ = "financial_org"
+    # set the column types
+    financial_org_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    summary = db.Column(db.String, unique=True)
+    city = db.Column(db.String)
+    companies = db.Column(db.String)
+    twitter = db.Column(db.String)
+    website = db.Column(db.String)
+    logo_url = db.Column(db.String)
+
+    # set the column values in constructor
+    #def __init__(self, name, summary, city, companies, twitter, website, logo_url):
+    def __init__(self, financial_org_id, name, summary, city, companies, twitter, website, logo_url):
+        self.financial_org_id = financial_org_id
+        self.name = name
+        self.summary = summary
+        self.city = city
+        self.companies = companies
+        self.twitter = twitter
+        self.website = website
+        self.logo_url = logo_url
+
+    def __repr__(self):
+        return '<FinancialOrg %r>' % self.name
+
+    def dictionary(self):
+        """Returns a dictionary representation of the class data"""
+        dict_rep = {}
+        dict_rep['financial_org_id'] = self.financial_org_id
+        dict_rep['name'] = self.name
+        dict_rep['summary'] = self.summary
+        dict_rep['city'] = self.city
+        dict_rep['companies'] = self.companies
+        dict_rep['twitter'] = self.twitter
+        dict_rep['website'] = self.website
+        dict_rep['logo_url'] = self.logo_url
+        return dict_rep
+
+
+class Person(db.Model):
+    __tablename__ = "person"
+    # set the column types
+    person_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    summary = db.Column(db.String, unique=True)
+    city = db.Column(db.String)
+    companies = db.Column(db.String)
+    role = db.Column(db.String)
+    twitter = db.Column(db.String)
+    logo_url = db.Column(db.String)
+
+    # set the column values in constructor
+    def __init__(self, person_id, name, summary, city, companies, role, twitter, logo_url):
+        self.person_id = person_id
+        self.name = name
+        self.summary = summary
+        self.city = city
+        self.companies = companies
+        self.role = role
+        self.twitter = twitter
+        self.logo_url = logo_url
+
+    def __repr__(self):
+        return '<Person %r>' % self.name
+
+    def dictionary(self):
+        """Returns a dictionary representation of the class data"""
+        dict_rep = {}
+        dict_rep['person_id'] = self.person_id
+        dict_rep['name'] = self.name
+        dict_rep['summary'] = self.summary
+        dict_rep['city'] = self.city
+        dict_rep['companies'] = self.companies
+        dict_rep['role'] = self.role
+        dict_rep['twitter'] = self.twitter
+        dict_rep['logo_url'] = self.logo_url
+        return dict_rep
+
+
+class City(db.Model):
+    __tablename__ = "city"
+    # set the column types
+    city_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    state = db.Column(db.String)
+    region = db.Column(db.String)
+    companies = db.Column(db.String)
+    financial_orgs = db.Column(db.String)
+    people = db.Column(db.String)
+
+    # set the column values in constructor
+    def __init__(self, city_id, name, state, region, companies, financial_orgs, people):
+        self.city_id = city_id
+        self.name = name
+        self.state = state
+        self.region = region
+        self.companies = companies
+        self.financial_orgs = financial_orgs
+        self.people = people
+
+    def __repr__(self):
+        return '<City %r>' % self.name
+
+    def dictionary(self):
+        """Returns a dictionary representation of the class data"""
+        dict_rep = {}
+        dict_rep['city_id'] = self.city_id
+        dict_rep['name'] = self.name
+        dict_rep['state'] = self.state
+        dict_rep['region'] = self.region
+        dict_rep['companies'] = self.companies
+        dict_rep['financial_orgs'] = self.financial_orgs
+        dict_rep['people'] = self.people
+        return dict_rep
---
+++ startupfairy/views.py
@@ -0,0 +1,58 @@
+"""
+Serves all the routes for the application
+"""
+import json
+from os import listdir
+from flask import render_template, Blueprint
+
+public_views = Blueprint('public_views', __name__)
+
+ALT_NAMES = {
+    'financialorgs': 'Financial Organizations',
+    'companies': 'Companies',
+    'cities': 'Cities',
+    'people': 'People'
+
+}
+
+
+@public_views.route('/')
+@public_views.route('/index')
+def index():
+    """
+    Serve the home/index page
+    """
+    return render_template('index.html')
+
+
+@public_views.route('/about')
+def about():
+    """
+    Serve the about page
+    """
+    return render_template('about.html', title='About')
+
+
+@public_views.route('/<category>', methods=['GET'])
+def category(app_category):
+    """Render table template"""
+    filepath = "static/data/%s" % (app_category)
+    data = []
+    for filename in listdir(filepath):
+        with open("%s/%s" % (filepath, filename), 'r') as json_data:
+            category_obj = json.load(json_data)
+            data.append(category_obj)
+    return render_template('category.html',
+                           alt_title=ALT_NAMES.get(app_category, None),
+                           title=category, data=data)
+
+
+@public_views.route('/<category>/<entity>', methods=['GET'])
+def details(app_category, entity):
+    """
+    Serve the an entity's page
+    """
+    filename = "static/data/%s/%s.json" % (app_category, entity)
+    with open(filename, 'r') as json_data:
+        data = json.load(json_data)
+    return render_template('details.html', data=data, category=app_category)
