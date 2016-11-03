8079577a257476148e17038bca22a647c259bc8b
Addy Kim <soyaddyk@gmail.com>
2016-11-02 19:50:58 -0500

    Load page without data

--- startupfairy/views.py
+++ startupfairy/views.py
@@ -34,26 +34,26 @@ def about():
     return render_template('about.html', alt_title='About')
 
 
-@public_views.route('/<category>', methods=['GET'])
+@public_views.route('/<app_category>', methods=['GET'])
 def category(app_category):
     """Render table template"""
     filepath = "static/data/%s" % (app_category)
     data = []
-    for filename in listdir(filepath):
-        with open("%s/%s" % (filepath, filename), 'r') as json_data:
-            category_obj = json.load(json_data)
-            data.append(category_obj)
+    # for filename in listdir(filepath):
+        # with open("%s/%s" % (filepath, filename), 'r') as json_data:
+            # category_obj = json.load(json_data)
+            # data.append(category_obj)
     return render_template('category.html',
                            alt_title=ALT_NAMES.get(app_category, None),
-                           title=category, data=data)
+                           title=app_category, data=data)
 
 
-@public_views.route('/<category>/<entity>', methods=['GET'])
+@public_views.route('/<app_category>/<entity>', methods=['GET'])
 def details(app_category, entity):
     """
     Serve the an entity's page
     """
-    filename = "static/data/%s/%s.json" % (app_category, entity)
-    with open(filename, 'r') as json_data:
-        data = json.load(json_data)
+    # filename = "static/data/%s/%s.json" % (app_category, entity)
+    # with open(filename, 'r') as json_data:
+        # data = json.load(json_data)
     return render_template('details.html', data=data, category=app_category)
